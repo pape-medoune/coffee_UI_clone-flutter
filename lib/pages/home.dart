eac172b10bf5ea868369769c67cdd9d6e92e1c78
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-09-27 14:15:00 +0000

    Starting Project

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,7 +24,7 @@ class Home extends StatelessWidget {
                       ),
                       child: const Icon(
                         Icons.menu,
-                        color: Colors.white,
+                        color: Color.fromARGB(87, 255, 255, 255),
                         size: 40,
                       ),
                     ),
@@ -37,12 +37,79 @@ class Home extends StatelessWidget {
                       ),
                       child: const Icon(
                         Icons.person,
-                        color: Colors.white,
+                        color: Color.fromARGB(87, 255, 255, 255),
                         size: 40,
                       ),
                     ),
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //TextFind..
+                const Row(
+                  children: [
+                    Text(
+                      "Find the best",
+                      style: TextStyle(
+                        fontSize: 38,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      "coffee for you",
+                      style: TextStyle(
+                        fontSize: 38,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //TextField
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 7,
+                      ),
+                      width: 367,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 17, 25, 29),
+                        border: Border.all(
+                          width: 1.0,
+                          color: Color.fromARGB(54, 229, 225, 225),
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Find your coffee',
+                          hintStyle: TextStyle(
+                            color: Color.fromARGB(115, 255, 255, 255),
+                            fontSize: 20,
+                          ),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color.fromARGB(87, 255, 255, 255),
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
